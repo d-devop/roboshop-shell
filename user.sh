857ece09b79c86c2f87efc3a16571bfffbfa2c9e
Divya <divya5guntaka@gmail.com>
2022-11-20 16:13:08 +0530

    User Script

--- user.sh
+++ user.sh
@@ -1,6 +1,5 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-
 useradd roboshop
 
 curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
@@ -11,7 +10,8 @@ mv user-main user
 cd /home/roboshop/user
 npm install
 
-# mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
-# systemctl daemon-reload
-# systemctl start user
-# systemctl enable user
+sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/MONGO_ENDPOINT/mongo.devopsb69.online/' systemd.service
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl restart user
+systemctl enable user
