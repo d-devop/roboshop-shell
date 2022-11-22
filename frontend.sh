7a0801e28618f5720e4f536f659844856160165a
Divya <divya5guntaka@gmail.com>
2022-11-22 13:43:37 +0530

    MySQL Script

--- frontend.sh
+++ frontend.sh
@@ -3,16 +3,23 @@ CONTENT="*"
 source common.sh
 
 PRINT "Install Nginx"
-yum install nginx -y &>>LOG
+yum install nginx -y &>>$LOG
 STAT $?
 
 APP_LOC=/usr/share/nginx/html
 
 DOWNLOAD_APP_CODE
-exit
 
 mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl restart nginx
+PRINT "Copy Roboshop Configuration File"
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+PRINT "Enable Nginx Service"
+systemctl enable nginx &>>$LOG
+STAT $?
+
+PRINT "Restart Nginx Service"
+systemctl restart nginx &>>$LOG
+STAT $?
