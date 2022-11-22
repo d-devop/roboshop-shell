89e6416e77a3e75851da1a962846bb997b697c06
Divya <divya5guntaka@gmail.com>
2022-11-22 15:45:16 +0530

    MySQL Script

--- common.sh
+++ common.sh
@@ -17,6 +17,15 @@ LOG=/tmp/$COMPONENT.log
 rm -f $LOG
 
 DOWNLOAD_APP_CODE(){
+ if [ ! -z "$APP_USER" ]; then
+  PRINT "Adding Application User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+      useradd roboshop &>>$LOG
+  fi
+  STAT $?
+ fi
+
   PRINT "Download App Content"
   curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>$LOG
   STAT $?
@@ -31,22 +40,34 @@ DOWNLOAD_APP_CODE(){
   STAT $?
 }
 
+SYSTEMD_SETUP(){
+   PRINT "Configure Endpoints for SystemD Configuration"
+    sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+    STAT $?
+
+    PRINT "Reload SystemD"
+    systemctl daemon-reload &>>$LOG
+    STAT $?
+
+    PRINT "Restart ${COMPONENT}"
+    systemctl restart ${COMPONENT} &>>$LOG
+    STAT $?
+
+    PRINT "Enable ${COMPONENT} Service"
+    systemctl enable ${COMPONENT} &>>$LOG
+    STAT $?
+}
+
 NODEJS(){
   APP_LOC=/home/roboshop
   CONTENT=$COMPONENT
+  APP_USER=roboshop
   PRINT "Install NodeJS Repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG
   STAT $?
 
   PRINT "Install NodeJS"
   yum install nodejs -y &>>$LOG
-  STAT $?
-
-  PRINT "Adding Application User"
-  id roboshop &>>$LOG
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>$LOG
-  fi
   STAT $?
 
   DOWNLOAD_APP_CODE
@@ -58,20 +79,22 @@ NODEJS(){
   npm install &>>$LOG
   STAT $?
 
-  PRINT "Configure Endpoints for SystemD Configuration"
-  sed -i -e 's/REDIS_ENDPOINT/redis.devopsb69.online/' -e 's/CATALOGUE_ENDPOINT/catalogue.devopsb69.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
-  STAT $?
+  SYSTEMD_SETUP
+}
 
-  PRINT "Reload SystemD"
-  systemctl daemon-reload &>>$LOG
-  STAT $?
+JAVA(){
+ APP_LOC=/home/roboshop
+ CONTENT=$COMPONENT
+ APP_USER=roboshop
 
-  PRINT "Restart ${COMPONENT}"
-  systemctl restart ${COMPONENT} &>>$LOG
-  STAT $?
+ PRINT "Install Maven"
+ yum install maven -y &>>$LOG
+ STAT $?
+ DOWNLOAD_APP_CODE
 
-  PRINT "Enable ${COMPONENT} Service"
-  systemctl enable ${COMPONENT} &>>$LOG
-  STAT $?
+ PRINT "Download Maven Dependencies"
+ mvn clean package &>>$LOG && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>$LOG
+ STAT $?
 
+ SYSTEMD_SETUP
 }
