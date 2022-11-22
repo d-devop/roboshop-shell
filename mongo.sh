16edebb93f78929deaeb374852e932ed6d9e59aa
Divya <divya5guntaka@gmail.com>
2022-11-22 14:30:59 +0530

    MySQL Script

--- mongo.sh
+++ mongo.sh
@@ -1,15 +1,42 @@
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
+COMPONENT=mongodb
+source common.sh
 
+PRINT "Download YUM Repo File"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG
+STAT $?
+
+PRINT "Install MongoDB"
 yum install -y mongodb-org
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
 
-systemctl enable mongod
-systemctl restart mongod
+PRINT "Configure MongoDB Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG
+STAT $?
 
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+PRINT "Enable MongoDB"
+systemctl enable mongod &>>$LOG
+STAT $?
 
+PRINT "Start MongoDB"
+systemctl restart mongod &>>$LOG
+STAT $?
+
+PRINT ""
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+STAT $?
 cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
+
+exit
+PRINT ""
+unzip -o mongodb.zip &>>$LOG
+STAT $?
+
+cd mongodb-main &>>$LOG
+
+PRINT ""
+mongo < catalogue.js &>>$LOG
+STAT $?
+
+PRINT ""
+mongo < users.js &>>$LOG
+STAT $?
