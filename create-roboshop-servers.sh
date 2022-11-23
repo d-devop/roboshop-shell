5c56997d69756e6f97c63770407ee58a39cb0bd4
Divya <divya5guntaka@gmail.com>
2022-11-23 14:06:19 +0530

    MySQL Script

--- create-roboshop-servers.sh
+++ create-roboshop-servers.sh
@@ -10,10 +10,10 @@ env=dev
 
 create_ec2(){
   PRIVATE_IP=$(aws ec2 run-instances \
-      --image-id ${AMI ID} \
+      --image-id ${AMI_ID} \
       --instance-type t3.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" \
-      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptBehavior=stop}" \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptBehavior=stop}"\
       --security-group-ids ${SGID} \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
   exit
