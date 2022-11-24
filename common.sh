a79e71b96e5c8a1389af91476ea4235f62f3b0d0
Divya <divya5guntaka@gmail.com>
2022-11-24 13:25:51 +0530

    MySQL Script

--- common.sh
+++ common.sh
@@ -1,30 +1,31 @@
-STAT(){
+STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
     echo Check the error in $LOG file
-    exit
+    exit 1
   fi
 }
 
 PRINT() {
-  echo "---------------------- $1 ------------------------" >> ${LOG}
+  echo " --------------------  $1 ------------------" >>${LOG}
   echo -e "\e[33m$1\e[0m"
 }
 
 LOG=/tmp/$COMPONENT.log
 rm -f $LOG
+set-hostname -skip-apply $COMPONENT
 
-DOWNLOAD_APP_CODE(){
- if [ ! -z "$APP_USER" ]; then
-  PRINT "Adding Application User"
-  id roboshop &>>$LOG
-  if [ $? -ne 0 ]; then
+DOWNLOAD_APP_CODE() {
+  if [ ! -z  "$APP_USER" ]; then
+    PRINT "Adding Application User"
+    id roboshop &>>$LOG
+    if [ $? -ne 0 ]; then
       useradd roboshop &>>$LOG
+    fi
+    STAT $?
   fi
-  STAT $?
- fi
 
   PRINT "Download App Content"
   curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/roboshop-devops-project/$COMPONENT/archive/main.zip" &>>$LOG
@@ -40,18 +41,18 @@ DOWNLOAD_APP_CODE(){
   STAT $?
 }
 
-SYSTEMD_SETUP(){
-    PRINT "Configure Endpoints for SystemD Configuration"
-    sed -i -e 's/MONGO_DNSNAME/dev-mongodb.devops-d.online/' -e 's/REDIS_ENDPOINT/dev-redis.devops-d.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.devops-d.online/' -e 's/MONGO_ENDPOINT/dev-mongodb.devops-d.online/' -e 's/CARTENDPOINT/dev-cart.devops-d.online/' -e 's/DBHOST/dev-mysql.devops-d.online/' -e 's/AMQPHOST/dev-rabbitmq.devops-d.online/' -e 's/CARTHOST/dev-cart.devops-d.online/' -e 's/USERHOST/dev-user.devops-d.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
-    mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-    STAT $?
+SYSTEMD_SETUP() {
+  PRINT "Configure Endpoints for SystemD Configuration"
+  sed -i -e 's/MONGO_DNSNAME/dev-mongodb.devops-d.online/' -e 's/REDIS_ENDPOINT/dev-redis.devops-d.online/' -e 's/CATALOGUE_ENDPOINT/dev-catalogue.devops-d.online/' -e 's/MONGO_ENDPOINT/dev-mongodb.devops-d.online/' -e 's/CARTENDPOINT/dev-cart.devops-d.online/' -e 's/DBHOST/dev-mysql.devops-d.online/' -e 's/AMQPHOST/dev-rabbitmq.devops-d.online/' -e 's/CARTHOST/dev-cart.devops-d.online/' -e 's/USERHOST/dev-user.devops-d.online/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG
+  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+  STAT $?
 
-    PRINT "Restart ${COMPONENT}"
-    systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
-    STAT $?
+  PRINT "Restart ${COMPONENT}"
+  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  STAT $?
 }
 
-NODEJS(){
+NODEJS() {
   APP_LOC=/home/roboshop
   CONTENT=$COMPONENT
   APP_USER=roboshop
@@ -63,37 +64,40 @@ NODEJS(){
   yum install nodejs -y &>>$LOG
   STAT $?
 
+
   DOWNLOAD_APP_CODE
 
   mv ${COMPONENT}-main ${COMPONENT}
   cd ${COMPONENT}
 
-  PRINT "Install NodeJS Dependencies for App"
+  PRINT "Install NodeJS Dependencies for App "
   npm install &>>$LOG
   STAT $?
 
   SYSTEMD_SETUP
 }
 
-JAVA(){
- APP_LOC=/home/roboshop
- CONTENT=$COMPONENT
- APP_USER=roboshop
 
- PRINT "Install Maven"
- yum install maven -y &>>$LOG
- STAT $?
+JAVA() {
+  APP_LOC=/home/roboshop
+  CONTENT=$COMPONENT
+  APP_USER=roboshop
 
- DOWNLOAD_APP_CODE
+  PRINT "Install Maven"
+  yum install maven -y  &>>$LOG
+  STAT $?
+
+  DOWNLOAD_APP_CODE
 
   mv ${COMPONENT}-main ${COMPONENT}
   cd ${COMPONENT}
 
   PRINT "Download Maven Dependencies"
-  mvn clean package &>>$LOG && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>$LOG
+  mvn clean package &>>$LOG  && mv target/$COMPONENT-1.0.jar $COMPONENT.jar &>>$LOG
   STAT $?
 
- SYSTEMD_SETUP
+  SYSTEMD_SETUP
+
 }
 
 PYTHON() {
