b5651e452b125e3ccdeb225c1452ba311bda9f70
Divya <divya5guntaka@gmail.com>
2022-11-24 12:40:27 +0530

    MySQL Script

---
+++ dispatch.sh
@@ -0,0 +1,22 @@
+COMPONENT=dispatch
+source common.sh
+
+PRINT "Install GoLang"
+yum install golang -y &>>$LOG
+STAT $?
+
+
+useradd roboshop
+curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+unzip /tmp/dispatch.zip
+mv dispatch-main dispatch
+cd dispatch
+go mod init dispatch
+go get
+go build
+
+mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+systemctl daemon-reload
+
+systemctl enable dispatch
+systemctl start dispatch
