0959eeb84eed2b0233afd04b0a0212575363780e
Divya <divya5guntaka@gmail.com>
2022-11-24 14:54:56 +0530

    Dispatch Script

--- dispatch.sh
+++ dispatch.sh
@@ -1,22 +1,22 @@
-COMPONENT=dispatch
-source common.sh
+#COMPONENT=dispatch
+#source common.sh
 
-PRINT "Install GoLang"
-yum install golang -y &>>$LOG
-STAT $?
+#PRINT "Install GoLang"
+#yum install golang -y &>>$LOG
+#STAT $?
 
 
-useradd roboshop
-curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
-unzip /tmp/dispatch.zip
-mv dispatch-main dispatch
-cd dispatch
-go mod init dispatch
-go get
-go build
+#useradd roboshop
+#curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+#unzip /tmp/dispatch.zip
+#mv dispatch-main dispatch
+#cd dispatch
+#go mod init dispatch
+#o get
+#go build
 
-mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
-systemctl daemon-reload
+#mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+#systemctl daemon-reload
 
-systemctl enable dispatch
-systemctl start dispatch
+#systemctl enable dispatch
+#systemctl start dispatch
